613b0a86f028ca8ccd3ffecbac974b79c7577254
Petr Vapenka <petr.vapenka@volny.cz>
2014-02-17 16:49:34 +0100

    Delete obsolete tests

--- test/query_builder/core_test.clj
+++ test/query_builder/core_test.clj
@@ -3,36 +3,6 @@
             [clojure.string]
             [query-builder.core :refer :all]))
 
-#_(deftest test-split-regex
-  (are [res sql] (= res (clojure.string/split sql *split-regex*))
-    ; splits at '?'
-    ["a = ", " and b = ", " and c = "]
-    "a = ? and b = ? and c = "
-
-    ; splits at ':keyword'
-    ["a = ", " and b = ", " and c = "]
-    "a = :key1 and b = :key2 and c = :key3" 
-
-    ; does not split at '::casts'
-    ["a = 1::text"]
-    "a = 1::text"
-
-    ; does not understand quotation
-    ["a = 'mistake", "' and b = 1"]
-    "a = 'mistake?' and b = 1"
-
-    ; does not understand quotation
-    ["a = 'part", "' and b = 1"]
-    "a = 'part:val' and b = 1"))
-
-#_(deftest test-placeholders
-  (are [res sql] (= res (placeholders *split-regex* sql))
-       [:? :key1 :? :key2]
-       "a = ? and b = :key1 and c in (?) and d = :key2"
-
-       []
-       "a = 1::text and b = 1"))
-
 (deftest test-substitute-with
   (are [param-value placeholders param-hash]
        (= param-value (substitute-with param-hash placeholders))
